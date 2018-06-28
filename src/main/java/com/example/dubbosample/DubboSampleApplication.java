c92a6814486b613a23091b87cf84e26b4bc99004
BzCoder <107630221@qq.com>
2018-06-28 10:30:20 +0800

    Initial commit

---
+++ src/main/java/com/example/dubbosample/DubboSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.dubbosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/dubbosample/DubboSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.dubbosample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DubboSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
